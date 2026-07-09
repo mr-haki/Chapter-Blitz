302506102bfa43fd57ebe24f3145e6a3a60214f6
mr-haki <immrhaki@gmail.com>
2026-07-09 13:27:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const searchInput = document.getElementById("searchInput");
+const recommendations = document.getElementById("recommendations");
+
+
+const mangaList = [
+    {
+        name: "One Piece",
+        link: "one-piece/index-one-piece.html",
+        description: "Chapter summaries, spoilers and discussions."
+    }
+];
+
+
+searchInput.addEventListener("input", function(){
+
+    let searchValue = searchInput.value.toLowerCase();
+
+    recommendations.innerHTML = "";
+
+
+    mangaList.forEach(function(manga){
+
+        if(manga.name.toLowerCase().includes(searchValue) && searchValue !== ""){
+
+
+            recommendations.innerHTML += `
+
+            <div class="recommendation-card">
+
+                <h3>
+                    <a href="${manga.link}">
+                        🏴‍☠️ ${manga.name}
+                    </a>
+                </h3>
+
+            </div>
+
+            `;
+
+        }
+
+    });
+
+});
